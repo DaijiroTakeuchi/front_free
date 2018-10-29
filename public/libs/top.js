5743c7cdb5256189406d03dbcb0ac79fd6f4f630
Daijiro Takeuchi <takeuchi@tam-bourine.co.jp>
2018-10-29 10:36:52 +0900

    トップ画面の微調整

--- public/libs/top.js
+++ public/libs/top.js
@@ -1,4 +1,4 @@
 // navigator.userAgent: 現在のブラウザのユーザエージェントを表す文字列を返す
 // toLowerCase: 文字列の中の大文字を小文字に変換した新しい文字列を返す
-var ua = navigator.userAgent.toLowerCase();
-console.log(ua);
+// var ua = navigator.userAgent.toLowerCase();
+// console.log(ua);
