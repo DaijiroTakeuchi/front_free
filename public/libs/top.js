c9735ba8edffd6d0e1c1b68c3d6b6dd1e6412a5a
Daijiro Takeuchi <takeuchi@tam-bourine.co.jp>
2018-10-30 16:02:02 +0900

    タブ切り替えの追加 *後日場所変動

--- public/libs/top.js
+++ public/libs/top.js
@@ -2,3 +2,16 @@
 // toLowerCase: 文字列の中の大文字を小文字に変換した新しい文字列を返す
 // var ua = navigator.userAgent.toLowerCase();
 // console.log(ua);
+
+// js (jQuery)
+// メディア タブ切り替え
+$('.mediaNavItem').on('click', function() {
+    // nav アクティブ切り替え
+    $('.mediaNavItem').removeClass('mediaNavItem-active');
+    $(this).addClass('mediaNavItem-active');
+
+    // body アクティブ切り替え
+    $('.mediaBodyItem').removeClass('mediaBodyItem-active');
+    var index = $(this).data('media');
+    $('.mediaBodyItem[data-media=' + index + ']').addClass('mediaBodyItem-active');
+});
